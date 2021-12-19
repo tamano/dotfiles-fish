f9ffbe925f6205cbaf8c18382e00d51d4abd61c3
Yuya TAMANO <965378+tamano@users.noreply.github.com>
2021-12-19 18:10:31 +0900

    fix bug

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -27,7 +27,7 @@ function attach_tmux_session_if_needed
     end
 
     set new_session "Create New Session" 
-    set ID (echo $ID\n$new_session | peco --on-cancel=error | cut -d: -f1)
+    set ID (tmux list-sessions | fzf --reverse | cut -d: -f1)
     if test "$ID" = "$new_session"
         tmux new-session
     else if test -n "$ID"
