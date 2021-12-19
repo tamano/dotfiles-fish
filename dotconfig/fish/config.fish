c509b4788af516495698545fd22adf5a220128d7
Yuya TAMANO <965378+tamano@users.noreply.github.com>
2021-12-19 17:42:06 +0900

    Autoload tmux

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -10,3 +10,24 @@ if status is-interactive
 end
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+# tmux
+function attach_tmux_session_if_needed
+    set ID (tmux list-sessions)
+    if test -z "$ID"
+        tmux new-session
+        return
+    end
+
+    set new_session "Create New Session" 
+    set ID (echo $ID\n$new_session | peco --on-cancel=error | cut -d: -f1)
+    if test "$ID" = "$new_session"
+        tmux new-session
+    else if test -n "$ID"
+        tmux attach-session -t "$ID"
+    end
+end
+
+if test -z $TMUX && status --is-login
+    attach_tmux_session_if_needed
+end
