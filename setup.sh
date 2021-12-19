cb9d4c66d233b531a17ac90e1d4a81d1313926ad
Yuya TAMANO <965378+tamano@users.noreply.github.com>
2021-12-19 12:54:12 +0900

    Add setup script for tool installing
    Fix #1 #3

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/opt/homebrew/bin/fish
+
+echo "🏎 Starting Setup!"
+
+echo "⏱️ Begin installing tools"
+
+# Install brew based tools
+brew install git
+brew install neovim
+brew install tig
+brew install tmux
+brew install gh
+brew install lsd
+brew install zoxide
+brew install fzf
+
+echo "✅ Complete installing tools"
+echo "⏱️ Begin linking dotfiles"
+echo "✅ Complete linking dotfiles"
+
+echo "🏁 All Complete!"
